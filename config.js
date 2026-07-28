03eb304f041fce10587416836bdf348ad3b4f804
5214441 <aiyaoyao11@gmail.com>
2026-07-28 22:52:29 +0800

    Update hotel configuration details

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 window.HOTEL_CONFIG = {
   hotelName: "K8智享酒店",
   shortName: "K8酒店",
-  address: "霍邱县",
-  wifiText: "请查看房间内Wi-Fi提示",
-  checkOutText: "请在退房前通过前台工单咨询",
+  address: "霍邱县城关镇城隍庙大街",
+  wifiText: "Wi-Fi名称：房间号，密码：123456789",
+  checkOutText: "退房时间：中午14:00前，房卡请交前台",
   // 部署 Cloudflare Worker 后，把下面地址改成你的 workers.dev 地址，不要以 / 结尾
   apiBase: "https://k8-api.kkkk8888.ccwu.cc",
   staffNames: ["前台A", "前台B"],
